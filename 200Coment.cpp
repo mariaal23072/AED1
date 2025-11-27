95382171d3e6afd9a49d3e07f22489a5a8450a3f
mariaal23072 <m.almarchalorca@um.es>
2025-11-27 13:33:04 +0100

    200 y 200 con comentarios

--- 200Coment.cpp
+++ 200Coment.cpp
@@ -150,6 +150,9 @@ bool Cuac::es_anterior(Cuac &otro) {
 
 // ============= 200 ==============
 
+
+class TablaHash; // para que Par pueda ser friend de TablaHash
+
 // CLASE PAR
 class Par { // "carpeta de cada usuario"
     friend class TablaHash;
@@ -179,7 +182,7 @@ TablaHash::TablaHash (){
 // Función hash
 int TablaHash::h (string k) {
     unsigned long res = 0;
-    for (unsigned int i = 0; i < k.size(); i++) { // unsigned para evitar error de comparación
+    for (int i = 0; i < k.size(); i++) {
         res = res * 67;
         res = res + k[i];
     }
@@ -190,8 +193,8 @@ void TablaHash::insertar(Cuac nuevo) {
     string usuario = nuevo.get_usuario();
     int pos = h(usuario); // calcular posición
 
-    list<Par>::iterator it;
-    for (it = T[pos].begin(); it != T[pos].end(); it++) {
+    list<Par>::iterator it = T[pos].begin();
+    for (it; it != T[pos].end(); it++) {
         if ( (*it).nombre == usuario ) {
             // Usuario encontrado
             list<Cuac>::iterator it2 = (*it).l.begin();
@@ -213,13 +216,13 @@ void TablaHash::insertar(Cuac nuevo) {
 void TablaHash::consultar (string nombre) {
     int pos = h(nombre); // calcular posición a consultar
 
-    list<Par>::iterator it;
-    for (it = T[pos].begin(); it != T[pos].end(); it++) { // recorrer lista de pares en la posición
+    list<Par>::iterator it = T[pos].begin();
+    for (it; it != T[pos].end(); it++) { // recorrer lista de pares en la posición
         if ( (*it).nombre == nombre ) {
             // Usuario encontrado
-            list<Cuac>::iterator it2;
+            list<Cuac>::iterator it2 = (*it).l.begin();
             int cont = 0; // contador de cuacs del usuario
-            for (it2 = (*it).l.begin(); it2 != (*it).l.end(); it2++) { // recorrer lista de cuacs del usuario
+            for (it2; it2 != (*it).l.end(); it2++) { // recorrer lista de cuacs del usuario
                 cout << (cont + 1) << ". ";
                 (*it2).escribir();
                 cont++;
@@ -239,6 +242,7 @@ void TablaHash::consultar (string nombre) {
 // ============= CLASE DEL 006 =============
 class DiccionarioCuacs {
 private:
+    // list<Cuac> lista;
     int contador;
     TablaHash tabla; // cambiado para 200
 public:
@@ -260,18 +264,59 @@ DiccionarioCuacs::DiccionarioCuacs (){
 
 void DiccionarioCuacs::insertar(Cuac nuevo){
     tabla.insertar(nuevo); // cambiado para 200
+    /**
+    list<Cuac>::iterator it = lista.begin();
+    while (it != lista.end() && (*it).es_anterior(nuevo)) {
+        it++;
+    }
+    lista.insert(it, nuevo);
+    **/
     contador++;
     
 }
 
 void DiccionarioCuacs::last(int N) {
+    /**int cont = 0;
+    list<Cuac>::iterator it;
+    for (it = lista.begin(); cont < N && it !=lista.end(); it++) {
+        cout << (cont + 1) << ". ";
+        (*it).escribir();
+        cont++;
+    }
+    cout << "Total: " << cont << " cuac" << endl;
+    **/
 }
 
 void DiccionarioCuacs::follow(string nombre) {
     tabla.consultar(nombre); // cambiado para 200
+    /**
+    int cont = 0;
+    list<Cuac>::iterator it;
+    for (it = lista.begin(); it !=lista.end(); it++) {
+        if ((*it).get_usuario() == nombre) {
+            cout << (cont + 1) << ". ";
+            (*it).escribir();
+            cont++;
+        }        
+    }
+    cout << "Total: " << cont << " cuac" << endl;
+    **/
 }
 
 void DiccionarioCuacs::date(Fecha f1, Fecha f2) {
+    /**
+    int cont = 0;
+    list<Cuac>::iterator it;
+    for (it = lista.begin(); it !=lista.end(); it++) {
+        // Si f1 < it.fecha <= f2
+        if (!((*it).get_fecha().es_menor(f1)) && ( ((*it).get_fecha().es_menor(f2)) || (*it).get_fecha().es_igual(f2) ) ) {
+            cout << (cont + 1) << ". ";
+            (*it).escribir();
+            cont++;
+        }
+    }
+    cout << "Total: " << cont << " cuac" << endl;
+    **/
 }
 // =========== FIN 006 ===========
 
